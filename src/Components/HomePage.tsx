0be5248c1fd971d16865b2b4deb60b99564cfd53
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2024-08-12 13:15:23 +0300

    home page

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@mui/material";
 import GlobalStates from "./GlobalState";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   const { Theme } = GlobalStates()
@@ -8,9 +9,9 @@ export default function HomePage() {
         <div className="container">
           <div className="row align-items-center">
             <div className="col-md-8 col-6">
-              <h2>Cook Like a pro with Our Eeasy and Tasty Recipes</h2>
+              <h2>Cook Like a pro with Our <span className="text-warning">Easy</span> and <span className="text-warning">Tasty</span> Recipes</h2>
               <p className="text-secondary">Quick and easy Recipes provided to you by the Cook and his assistant Jessie</p>
-              <Button sx={{color:'white'}} variant="contained" color="warning">Explore all Recipes</Button>
+              <Button component={Link} to={'/recipes'} sx={{color:'white'}} variant="contained" color="warning">Explore all Recipes</Button>
             </div>
             <div className="col-md-4 col-6">
             <img className="w-100" src="https://ssniper.sirv.com/Images/Recipe%20Site/compressed-image.jpg" alt="Hero Image" />
